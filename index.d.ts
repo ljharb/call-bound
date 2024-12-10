1a0c9fe3114471e7ca1f57d104e2efe713bb4871
Jordan Harband <ljharb@gmail.com>
2024-12-10 08:42:27 -0800

    [types] improve return type

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-import callBind from 'call-bind';
+import callBind from 'call-bind-apply-helpers';
 
 declare function callBoundIntrinsic(name: string, allowMissing?: boolean): ReturnType<typeof callBind>;
 
